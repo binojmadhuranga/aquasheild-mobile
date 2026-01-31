a8c7a0af065ed18a335a4b5f89f6f5ca0cd694a9
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-01-31 15:47:37 +0530

    chore: update expo to version 54.0.32 and react-native-screens to version 4.16.0

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-function index() {
+function Index() {
   return (
     <View style={styles.container}>
-      <Text>index</Text>
+      <Text>Index</Text>
     </View>
   )
 }
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export default index
+export default Index
