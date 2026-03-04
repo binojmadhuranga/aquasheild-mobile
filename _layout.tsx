d8b0a0eb042a65a068db1b93c8b12cfe1c684b8d
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-03-04 08:35:59 +0530

    feat: restructure services and add authentication context
    - Removed old authService and axiosConfig files and created new versions in the src/services directory.
    - Added sensorService and storage utility files.
    - Introduced AuthContext for managing authentication state.
    - Updated package.json to include nativewind and tailwindcss for styling.
    - Created global CSS file for Tailwind CSS integration.
    - Configured Babel to support NativeWind.
    - Added Tailwind CSS configuration file.
    - Implemented a new layout component to wrap the application with AuthProvider.

---
+++ _layout.tsx
@@ -0,0 +1,14 @@
+import '../global.css';
+import { Stack } from 'expo-router';
+import AuthProvider from './src/context/AuthContext';
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </AuthProvider>
+  );
+}
